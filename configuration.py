c49a77b0116b6196083e84d9a23e17b23a6997bd
Alla <allastulova83@gmail.ru>
2024-11-25 22:01:38 +0300

    Add commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE =  " https://9da5f9cc-3eca-4036-bd68-f4f7b5a144c2.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_BY_TRACK_PATH = "/api/v1/orders/track"
